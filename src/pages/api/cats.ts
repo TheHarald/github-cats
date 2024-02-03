5ec3a3259488526e168bfecd9205b0a91161ae28
Roman Surinov <romasurinov13@mail.ru>
2024-02-04 02:56:47 +0300

    add cow

--- src/pages/api/cats.ts
+++ src/pages/api/cats.ts
@@ -1,16 +1,15 @@
+import { cow } from "@/svgs/cow";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const svgContent = `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-<circle cx="30" cy="30" r="20" fill="blue">
-  <animate
-    attributeName="cy"
-    values="30;170;30"
-    dur="2s"
-    keyTimes="0;0.5;1"
-    repeatCount="indefinite"
-  />
-</circle>
-</svg>`;
+const svgContent = `
+<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
+  <!-- Initial frame -->
+  <image href="/cow0.svg" width="100%" height="100%">
+
+    <animate attributeName="href" values="/cow1.svg;/cow2.svg;/cow1.svg" dur="1s" repeatCount="indefinite" />
+  </image>
+</svg>
+`;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader("Content-Type", "image/svg+xml");
